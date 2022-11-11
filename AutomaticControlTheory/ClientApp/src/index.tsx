890148606955eb2bb4377118f3da6a00437888f5
boloto51 <boloto51@mail.ru>
2022-11-11 15:57:19 +0300

    Коммит не отправился предыдущий?

---
+++ AutomaticControlTheory/ClientApp/src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom";
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>AAA</div>
+        <div>AAA</div>
+    </BrowserRouter>
+    , document.getElementById('root'));
